b7dc3e114272a3955604b1f734a9d8fc565ddd19
Ola Ekdahl <ola@ciracon.com>
2023-01-24 06:44:56 -0800

    index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log('Hello!');
  // noncompliant code
  // a user can craft an HTTP request with malicious content
 document.location = document.location.hash.slice(1);
+let i = 1;
+
+if(i==1) {
+    console.log('the same')
+}
+alert('hi');
